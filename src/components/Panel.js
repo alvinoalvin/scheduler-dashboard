964cde7ed06b57b69a37e1d375d0933c7a1b800c
alvinoalvin <alvinoalvin@gmail.com>
2021-05-20 01:32:37 -0700

    adding websockets

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { id, label, value, onSelect } = this.props;
+    const { key, label, value, onSelect } = this.props;
 
     return (
-      <section className="dashboard__panel" onClick={event => onSelect(id)}>
+      <section className="dashboard__panel" onClick={event => onSelect(key)}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
